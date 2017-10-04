8dac316f8d3d23b4bacd13eeeed5cd6350c3f2ff
krysmathis <krysmathis@gmail.com>
2017-10-04 08:56:12 -0500

    added a second sentence

--- overly-excited.js
+++ overly-excited.js
@@ -2,20 +2,31 @@
 var walrus = "The thunder danced through the trees in the light of the moon";
 var sentence = walrus.split(" ");
 
+var fox = "The quick brown fox jumps over the lazy dog";
+var nextSentence = fox.split(" ");
 /*
     The addExcitement function should be an impure function, and accept 
     the array as the sole argument. It should iterate over the array 
     and output the words to the browser console.
 */
-function addExcitement (theWordArray) {
+function addExcitement (theWordArray, displayChar) {
+    
     var words = "";
+    var c = 1;
+
+    //  When we reach the third iteration add the display char
+    //  Repeat the display char based on how many times you can divide it by 3
 
     for ( var i = 0; i < theWordArray.length; i += 1 ) {
-        words += theWordArray[i] + " ";
-        console.log(words.trim());
-        
+        if (c % 3 === 0) {
+            words += theWordArray[i] + displayChar.repeat(c/3) + " ";
+        } else {
+            words += theWordArray[i] + " ";
+        }
+        console.log(words.trim())
+        c += 1;
     }
 }
 
 // Invoke the function and pass in the array
-addExcitement(sentence);
+addExcitement(nextSentence, "?");
